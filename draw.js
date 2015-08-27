2592bf158460cca545a6bed7ef7279cb3b38b8f6
rudenoise <rudenoise@gmail.com>
2015-08-27 16:15:15 +0100

    fill lines

--- draw.js
+++ draw.js
@@ -4,7 +4,23 @@ var coords = require('./coordinates.json');
     var grid = [], y = 0;
 
     function drawLine(from, to, grid, char) {
-        
+        var x, y;
+        if (from[0] === to[0]) {
+            x = from[0];
+            y = from[1]
+            while(y < to[1]) {
+                grid[y][x] = char;
+                y += 1;
+            }
+        }
+        if (from[1] === to[1]) {
+            x = from[0];
+            y = from[1]
+            while(x < to[0]) {
+                grid[y][x] = char;
+                x += 1;
+            }
+        }
     }
 
     function plot(grid, path, char) {
@@ -13,6 +29,15 @@ var coords = require('./coordinates.json');
             next = path[i];
             if (last && next) {
                 // draw line
+                if (last[0] === next[0]) {
+                    last[1] < next[1] ?
+                        drawLine(last, next, grid, char) :
+                        drawLine(next, last, grid, char);
+                } else {
+                    last[0] < next[0] ?
+                        drawLine(last, next, grid, char) :
+                        drawLine(next, last, grid, char);
+                }
             }
             grid[next[1]][next[0]] = char;
             last = next;
